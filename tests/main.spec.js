36f2bce7ed5ba9481533078d123bfcbdc87a2e58
Rafael F. Assumpcao <rafaelfassumpcao@gmail.com>
2018-02-18 20:56:20 -0300

    added testing coverage

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -2,38 +2,38 @@ import { expect } from 'chai';
 import fizzBuzz from '../src/main';
 
 describe('Main', () => {
-  /*  describe('smoke tests', () => {
-     it('should exist function fizzbuzz', () => {
-       expect(fizzBuzz).to.exist;
-       expect(fizzBuzz).to.be.a('function');
-     });
-   });
- 
-   describe('FizzBuzz function', () => {
-     it('should return \'Fizz\' when fizzBuzz(num) is of 3 but not of 5', () => {
-       expect(fizzBuzz(6)).to.be.an('string');
-       expect(fizzBuzz(6)).to.be.equal('Fizz');
-     });
- 
-     it('should return \'Buzz\' when fizzBuzz(num) is multiple of 5 but not of 3', () => {
-       expect(fizzBuzz(10)).to.be.an('string');
-       expect(fizzBuzz(10)).to.be.equal('Buzz');
-       expect(fizzBuzz(20)).to.be.equal('Buzz');
-     });
- 
-     it('should return FizzBuzz when fizzBuzz(num) is multiple of both 3 and 5', () => {
-       expect(fizzBuzz(30)).to.be.an('string');
-       expect(fizzBuzz(15)).to.be.equal('FizzBuzz');
-       expect(fizzBuzz(30)).to.be.equal('FizzBuzz');
-     });
- 
-     it('should return num when fizzBuzz(num) is not multiple of 5 neither of 3', () => {
-       expect(fizzBuzz(13)).to.be.an('Number');
-       expect(fizzBuzz(13)).to.be.equal(13);
-     });
- 
-     it('should return 0 when fizzBuzz(0)', () => {
-       expect(fizzBuzz(0)).to.be.equal(0);
-     });
-   }); */
+  describe('smoke tests', () => {
+    it('should exist function fizzbuzz', () => {
+      expect(fizzBuzz).to.exist;
+      expect(fizzBuzz).to.be.a('function');
+    });
+  });
+
+  describe('FizzBuzz function', () => {
+    it('should return \'Fizz\' when fizzBuzz(num) is of 3 but not of 5', () => {
+      expect(fizzBuzz(6)).to.be.an('string');
+      expect(fizzBuzz(6)).to.be.equal('Fizz');
+    });
+
+    it('should return \'Buzz\' when fizzBuzz(num) is multiple of 5 but not of 3', () => {
+      expect(fizzBuzz(10)).to.be.an('string');
+      expect(fizzBuzz(10)).to.be.equal('Buzz');
+      expect(fizzBuzz(20)).to.be.equal('Buzz');
+    });
+
+    it('should return FizzBuzz when fizzBuzz(num) is multiple of both 3 and 5', () => {
+      expect(fizzBuzz(30)).to.be.an('string');
+      expect(fizzBuzz(15)).to.be.equal('FizzBuzz');
+      expect(fizzBuzz(30)).to.be.equal('FizzBuzz');
+    });
+
+    it('should return num when fizzBuzz(num) is not multiple of 5 neither of 3', () => {
+      expect(fizzBuzz(13)).to.be.an('Number');
+      expect(fizzBuzz(13)).to.be.equal(13);
+    });
+
+    it('should return 0 when fizzBuzz(0)', () => {
+      expect(fizzBuzz(0)).to.be.equal(0);
+    });
+  });
 });
